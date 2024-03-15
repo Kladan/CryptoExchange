851cf9abe6e6283de49fd49a4dfe51fa70896b25
Adrian <acc@kladan.de>
2024-03-15 12:56:29 +0100

    Consider available funds of crypto exchange

--- Program.cs
+++ Program.cs
@@ -29,7 +29,6 @@ internal class Program
 
         string json = JsonSerializer.Serialize(result);
         Console.WriteLine(json);
-        Console.WriteLine(result.Sum(o => o.Price));
     }
 
     private static bool ValidateArguments(string type, decimal goal, List<string> fileList)
@@ -82,23 +81,31 @@ internal class Program
 
         foreach (Exchange exchange in exchanges)
         {
+            decimal fund = 0;
             List<Order> exchangeOrders = new List<Order>();
             if (orderType == OrderType.Buy)
             {
+                fund = exchange.AvailableFunds.Crypto;
                 exchangeOrders = exchange.OrderBook.Asks.ConvertAll(a => a.Order);
                 exchangeOrders.Sort((a, b) => a.Price < b.Price ? -1 : a.Price == b.Price ? 0 : 1);
             }
             else if (orderType == OrderType.Sell)
             {
+                fund = exchange.AvailableFunds.Euro;
                 exchangeOrders = exchange.OrderBook.Bids.ConvertAll((b => b.Order));
                 exchangeOrders.Sort((a, b) => a.Price > b.Price ? -1 : a.Price == b.Price ? 0 : 1);
             }
 
-            List<Order> bestExchangeOrders = GetBestOrdersFromList(exchangeOrders, goalAmount);
-            decimal exchangeSum = bestExchangeOrders.Sum(a => a.Price);
-            decimal resultSum = result.Sum(a => a.Price);
+            List<Order> bestExchangeOrders = GetBestOrdersFromList(exchangeOrders, goalAmount, fund, orderType);
+            decimal exchangeSumEur = bestExchangeOrders.Sum(a => a.Price);
+            decimal exchangeSumBtc = bestExchangeOrders.Sum(a => a.Amount);
+            decimal bestResultSumEur = result.Sum(a => a.Price);
+            decimal bestResultSumBtc = result.Sum(a => a.Amount);
 
-            if (result.Count == 0 || exchangeSum < resultSum)
+            if (result.Count == 0 ||
+                (orderType == OrderType.Buy && exchangeSumEur < bestResultSumEur ||
+                 orderType == OrderType.Sell && exchangeSumEur > bestResultSumEur) &&
+                exchangeSumBtc >= bestResultSumBtc)
             {
                 result = bestExchangeOrders;
             }
@@ -107,33 +114,68 @@ internal class Program
         return result;
     }
 
-    private static List<Order> GetBestOrdersFromList(List<Order> orders, decimal goalAmount)
+    private static List<Order> GetBestOrdersFromList(List<Order> orders, decimal goalAmount,
+        decimal availableFund, OrderType orderType)
     {
         List<Order> bestOrders = new List<Order>();
-        decimal reachedAmount = 0;
+        decimal reachedBtc = 0;
+        decimal reachedEur = 0;
 
         foreach (Order order in orders)
         {
-            if (reachedAmount + order.Amount < goalAmount)
+            bool isLastOrder = PrepareOrder(order, goalAmount, availableFund, orderType, reachedBtc, reachedEur);
+
+            bestOrders.Add(order);
+            reachedBtc += order.Amount;
+            reachedEur += order.Amount * order.Price;
+
+            bool isRequestedAmountHit = reachedBtc + order.Amount == goalAmount;
+            bool isBuyableFundHit = orderType == OrderType.Buy && reachedBtc + order.Amount == availableFund;
+            bool isSellableFundHit =
+                orderType == OrderType.Sell && reachedEur + order.Amount * order.Price == availableFund;
+
+            if (isRequestedAmountHit || isBuyableFundHit || isSellableFundHit)
             {
-                bestOrders.Add(order);
-                reachedAmount += order.Amount;
+                isLastOrder = true;
             }
-            else if (reachedAmount + order.Amount == goalAmount)
-            {
-                bestOrders.Add(order);
-                break;
-            }
-            else if (reachedAmount + order.Amount > goalAmount)
-            {
-                decimal splittedAmount = goalAmount - reachedAmount;
-                Order splittedOrder = order;
-                splittedOrder.Amount = splittedAmount;
-                bestOrders.Add(splittedOrder);
-                break;
-            }
+
+            if (isLastOrder) break;
         }
 
         return bestOrders;
+    }
+
+    private static bool PrepareOrder(Order order, decimal goalAmount, decimal availableFund, OrderType orderType,
+        decimal reachedBtc, decimal reachedEur)
+    {
+        bool isLastOrder = false;
+        
+        bool wouldOverrunRequestedAmount = reachedBtc + order.Amount > goalAmount;
+        if (wouldOverrunRequestedAmount)
+        {
+            decimal restAmount = goalAmount - reachedBtc;
+            order.Amount = restAmount;
+            isLastOrder = true;
+        }
+        
+        bool wouldOverrunBuyableFund = orderType == OrderType.Buy && reachedBtc + order.Amount > availableFund;
+        if (wouldOverrunBuyableFund)
+        {
+            decimal restAmount = availableFund - reachedBtc;
+            order.Amount = restAmount;
+            isLastOrder = true;
+        }
+
+        bool wouldOverrunSellableFund = orderType == OrderType.Sell &&
+                                        reachedEur + order.Amount * order.Price > availableFund;
+        if (wouldOverrunSellableFund)
+        {
+            decimal restEur = availableFund - reachedEur;
+            decimal d = order.Price / restEur;
+            order.Amount = 1 / d;
+            isLastOrder = true;
+        }
+
+        return isLastOrder;
     }
 }
